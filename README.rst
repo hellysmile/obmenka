612479f3c6b88f0c5eb6d5d9ac620c92ca30a9f4
hellysmile <hellysmile@gmail.com>
2015-02-26 14:00:38 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,18 @@
+Installation Mac OS X:
+**********************
+
+.. code-block:: bash
+
+    brew brew install libxml2 libxslt
+
+    sudo pip install virtualenv
+
+Usage:
+******
+
+.. code-block:: bash
+
+    virtualenv env
+    source env/bin/activate
+    pip install -r requirements.txt
+    python main.py
---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import time
+
+import requests
+
+from lxml.html import fromstring
+from pync import Notifier
+
+ENDPOINT = 'http://obmenka.kharkov.ua/'
+TIMEOUT = 5
+XPATH = '/html/body/div[2]/div[2]/div[1]/table/tbody/tr[1]/td[5]'
+TITLE = 'currency UAH per USD'
+
+
+def main():
+    last_currency = None
+
+    while True:
+        try:
+            html = requests.get(ENDPOINT).text
+            tree = fromstring(html)
+            el = tree.xpath(XPATH)[0]
+            value = float(el.text.replace(',', '.'))
+        except Exception:
+            time.sleep(TIMEOUT)
+            continue
+
+        if last_currency != value:
+            last_currency = value
+
+            try:
+                Notifier.notify(
+                    str(value),
+                    open=ENDPOINT,
+                    title=TITLE
+                )
+            except Exception:
+                pass
+
+        time.sleep(TIMEOUT)
+
+
+if __name__ == '__main__':
+    main()
